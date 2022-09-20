1a95bde9c7eeb0e8708c6d9d489002bb8edc9f3b
Rasel <mdraselm413@gmail.com>
2022-09-20 23:10:31 +0600

    final code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
     </div>
   );
 }
 
-function Counter(){
+function LoadComments() {
+  const [comments, setComments] = useState([])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  }, [] )
+
+  return (
+    <div>
+      <h2>Total Comments: {comments.length}</h2>
+      {
+        comments.map(comment => <Comment email = {comment.email} body = {comment.body}></Comment> )
+      }
+    </div>
+  )
+}
+
+function Comment (props){
   return(
     <div>
-      <h1>Counter</h1>
-    <button>Increase</button>
-    <button>Decrease</button>
+      <h4>Email ID: {props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return(
+    <div>
+      <h1>Counter: {count}</h1>
+    <button onClick={handleIncrease}>Increase</button>
+    <button onClick={handleDecrease}>Decrease</button>
     </div>
   )
 }
