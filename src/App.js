64cfe6306ccf0c96006daa3834513a34924d7d7d
Rasel <mdraselm413@gmail.com>
2022-09-20 22:19:49 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+function Counter(){
+  return(
+    <div>
+      <h1>Counter</h1>
+    <button>Increase</button>
+    <button>Decrease</button>
+    </div>
+  )
+}
+export default App;
